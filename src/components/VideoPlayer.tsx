1722d61daf9de6b120307761aa412e7f11d0bfc2
Evgeniy568Programmist <159463375+Evgeniy568Programmist@users.noreply.github.com>
2025-02-01 20:40:07 +0300

    Update VideoPlayer.tsx

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -197,14 +197,14 @@ const VideoPlayer = ({
       
       {/* Progress bar */}
       <div 
-        className="absolute bottom-20 left-0 right-0 h-1 bg-gray-600 cursor-pointer"
+        className="absolute bottom-0 left-0 right-0 h-1 bg-gray-600 cursor-pointer"
         onClick={handleProgressClick}
       >
         <div 
           className="h-full bg-tiktok-red transition-all duration-100"
           style={{
             width: `${progress}%`,
-            transform: 'translateY(-50%)'
+            transform: 'translateY(-25%)'
           }}
         />
       </div>
